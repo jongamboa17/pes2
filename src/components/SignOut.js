2e8bfe063f1f6c9d56d87248d9d7b41d292e18b8
Jonathan Gamboa <jongamboa@gmail.com>
2023-11-30 05:10:59 -0600

    arreglo responsive navbar

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -16,7 +16,7 @@ export default function SignOut() {
   }
 
   return (
-    
+    <>
     <button type="button" 
             class=" text-white  hover:bg-green-400 focus:ring-4 
                     focus:outline-none focus:ring-white font-medium 
@@ -30,7 +30,9 @@ export default function SignOut() {
     </svg>
     <span class="sr-only">Icon description</span>
   </button>
+
   
+  </>
   );
 }
 
